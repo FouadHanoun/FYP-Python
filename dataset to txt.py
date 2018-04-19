f476852c42348adb89cf49ef31ce2f3c47f7bfe8
fhanoun <hanounfouad@gmail.com>
2018-04-19 10:57:38 +0200

    Automatic feature loading, added real time analysis, added feature to convert kinect data to text file for the dataset completion

--- dataset to txt.py
+++ dataset to txt.py
@@ -1,5 +1,24 @@
 
-''' Writing the dataset in text
+# Writing the dataset in text
+
+filename = 'C:\\Users\\fouad.hannoun\\AppData\\Local\\Packages\\a4548d07-af25-4b98-b7b4-ad4c4798fc82_q8p7fyft9r39a\\LocalState\\Test Folder\\sample.txt'
+file = open(filename,'r')
+features = []
+i=0
+j=0
+features.append([])
+for line in file:
+    if (":" not in line):
+        features[j].append(float(line.split(" ")[1].strip('\n')))
+    i+=1
+    if (i==20):
+        print(features[j])
+        features.append([])
+        i=0
+        j+=1
+del features[-1]
+
+
 f= open("dataset.txt","w+")
 u=0
 f.write('[')
@@ -14,7 +33,5 @@ for z in features:
         break
     f.write('\n')
 f.write(']')
-z=f.read()
 f.close()
-print(z)
-'''
+
